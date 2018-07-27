0f17c78d9f977575faaa72ff0998bc741a73c916
JEMD <x_jemd_x@hotmail.com>
2018-07-27 15:23:14 -0400

    content-type agregado

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cors());
 // Headers
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, content-type");
     next();
 });
 
