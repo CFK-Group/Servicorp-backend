151d57e34c66df0b647900ca591b6a6970efb01a
JEMD <x_jemd_x@hotmail.com>
2018-08-01 12:51:17 -0400

    probando headers nuevos

--- src/app.js
+++ src/app.js
@@ -18,8 +18,13 @@ app.use(cors())
 
 // Headers
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, content-type")
+    //res.header("Access-Control-Allow-Origin", "*")
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, content-type")
+
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Credentials", "true")
+    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     next()
 })
 
