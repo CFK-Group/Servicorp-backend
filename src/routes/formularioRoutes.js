cde899d3048d592e14b9a59f51ff8d200a902104
JEMD <x_jemd_x@hotmail.com>
2018-08-22 00:53:14 -0300

    agregando rutas para formularios de entel

--- src/routes/formularioRoutes.js
+++ src/routes/formularioRoutes.js
@@ -4,8 +4,10 @@ const global = require('../middlewares/auth')
 module.exports = (app) => {
     let statusError = 500
 
+    //***************************Formularios Claro***************************
+
     // Formulario de mantencion hfc id=1
-    app.post('/formulario/mantencion/hfc', (req, res) => {
+    app.post('/formulario/claro/mantencion/hfc', (req, res) => {
         const respuestas = {
             ot_servicorp: req.body.ot_servicorp,
             folio_servicio: req.body.folio_servicio,
@@ -154,7 +156,7 @@ module.exports = (app) => {
     })
 
     // Formulario de mantencion dth id=2
-    app.post('/formulario/mantencion/dth', (req, res) => {
+    app.post('/formulario/claro/mantencion/dth', (req, res) => {
         const respuestas = {
             ot_servicorp: req.body.ot_servicorp,
             folio_servicio: req.body.folio_servicio,
@@ -302,7 +304,7 @@ module.exports = (app) => {
     })
 
     // Formulario de instalacion hfc id=3
-    app.post('/formulario/instalacion/hfc', (req, res) => {
+    app.post('/formulario/claro/instalacion/hfc', (req, res) => {
         const respuestas = {
             ot_servicorp: req.body.ot_servicorp,
             folio_servicio: req.body.folio_servicio,
@@ -451,7 +453,7 @@ module.exports = (app) => {
     })
 
     // Formulario de instalacion dth id=4
-    app.post('/formulario/instalacion/dth', (req, res) => {
+    app.post('/formulario/claro/instalacion/dth', (req, res) => {
         const respuestas = {
             ot_servicorp: req.body.ot_servicorp,
             folio_servicio: req.body.folio_servicio,
@@ -598,9 +600,9 @@ module.exports = (app) => {
                 })
             })
     })
-
+    
     // Formulario de desconexion id=5
-    app.post('/formulario/desconexion', (req, res) => {
+    app.post('/formulario/claro/desconexion', (req, res) => {
         const respuestas = {
             ot_servicorp: req.body.ot_servicorp,
             folio_servicio: req.body.folio_servicio,
@@ -666,21 +668,170 @@ module.exports = (app) => {
                 })
             })
     })
+    
+    //***************************Formularios Entel***************************
+
+    // Formulario de instalacion dth id=6
+    app.post('/formulario/entel/instalacion/dth', (req, res) => {
+        const respuestas = {
+            ot_servicorp: req.body.ot_servicorp,
+            folio_servicio: req.body.folio_servicio,
+            resp_1: req.body.resp_1,
+            resp_2: req.body.resp_2,
+            resp_3: req.body.resp_3,
+            resp_4: req.body.resp_4,
+            resp_5: req.body.resp_5,
+            resp_6: req.body.resp_6,
+            resp_7: req.body.resp_7,
+            resp_8: req.body.resp_8,
+            resp_9: req.body.resp_9,
+            resp_10: req.body.resp_10,
+            resp_11: req.body.resp_11,
+            resp_12: req.body.resp_12,
+            resp_13: req.body.resp_13,
+            resp_14: req.body.resp_14,
+            resp_15: req.body.resp_15,
+            resp_16: req.body.resp_16,
+            resp_17: req.body.resp_17,
+            resp_18: req.body.resp_18,
+            resp_19: req.body.resp_19,
+            resp_20: req.body.resp_20,
+            resp_21: req.body.resp_21,
+            resp_22: req.body.resp_22,
+            resp_23: req.body.resp_23,
+            resp_24: req.body.resp_24,
+            resp_25: req.body.resp_25,
+            resp_26: req.body.resp_26,
+            resp_27: req.body.resp_27,
+            resp_28: req.body.resp_28,
+            resp_29: req.body.resp_29,
+            resp_30: req.body.resp_30,
+            resp_31: req.body.resp_31,
+            resp_32: req.body.resp_32,
+            resp_33: req.body.resp_33,
+            resp_34: req.body.resp_34,
+            resp_35: req.body.resp_35,
+            resp_36: req.body.resp_36,
+            resp_37: req.body.resp_37,
+            resp_38: req.body.resp_38,
+            resp_39: req.body.resp_39,
+            resp_40: req.body.resp_40,
+            resp_41: req.body.resp_41,
+            resp_42: req.body.resp_42,
+            resp_43: req.body.resp_43,
+            resp_44: req.body.resp_44,
+            resp_45: req.body.resp_45,
+            resp_46: req.body.resp_46,
+            resp_47: req.body.resp_47,
+            resp_48: req.body.resp_48,
+            resp_49: req.body.resp_49,
+            resp_50: req.body.resp_50,
+            resp_51: req.body.resp_51,
+            resp_52: req.body.resp_52,
+            resp_53: req.body.resp_53,
+            resp_54: req.body.resp_54,
+            resp_55: req.body.resp_55,
+            resp_56: req.body.resp_56,
+            resp_57: req.body.resp_57,
+            resp_58: req.body.resp_58,
+            resp_59: req.body.resp_59,
+            resp_60: req.body.resp_60,
+            resp_61: req.body.resp_61,
+            resp_62: req.body.resp_62,
+            resp_63: req.body.resp_63,
+            resp_64: req.body.resp_64,
+            resp_65: req.body.resp_65,
+            resp_66: req.body.resp_66,
+            resp_67: req.body.resp_67,
+            resp_68: req.body.resp_68,
+            resp_69: req.body.resp_69,
+            resp_70: req.body.resp_70,
+            resp_71: req.body.resp_71,
+            resp_72: req.body.resp_72,
+            resp_73: req.body.resp_73,
+            resp_74: req.body.resp_74,
+            resp_75: req.body.resp_75,
+            resp_76: req.body.resp_76,
+            resp_77: req.body.resp_77,
+            resp_78: req.body.resp_78,
+            resp_79: req.body.resp_79,
+            resp_80: req.body.resp_80,
+            resp_81: req.body.resp_81,
+            resp_82: req.body.resp_82,
+            resp_83: req.body.resp_83,
+            resp_84: req.body.resp_84,
+            resp_85: req.body.resp_85,
+            cod_decodificador: req.body.cod_decodificador,
+            imagen_1: req.body.imagen_1,
+            imagen_2: req.body.imagen_2,
+            imagen_3: req.body.imagen_3,
+            imagen_4: req.body.imagen_4,
+            latitud: req.body.latitud,
+            longitud: req.body.longitud,
+            tipo_formulario_id: 6,
+            usuario_id: null
+        }
+        let auth = new Promise ( (resolve, reject) => {
+            global.validateToken(req.body.token, (response, err) => {
+                if(!err){
+                    console.log('usuario autorizado')
+                    respuestas.usuario_id = response.userId
+                    return resolve(true)
+                }else{
+                    statusError = 401
+                    reject(new Error('Token inválido'))
+                }
+            })
+        })
+
+        auth
+            // creamos el formulario, guardamos respuestas e imágenes
+            .then( (resolved, rejected) => {
+                return new Promise( (resolve, reject) => {
+                    formulario.createForm(respuestas, (err, res) => {
+                        console.log('-----------------------------')
+                        console.log(err)
+                        console.log(res)
+                        console.log('-----------------------------')
+                        return (err) ? reject(new Error('No se ha podido crear un nuevo formulario')) : resolve(res)
+                    })
+                })
+            })
+
+            // respondemos al cliente
+            .then( (resolved, rejected) => {
+                res.status(200).json({
+                    success: true,
+                    message: `Formulario guardado con éxito`
+                })
+            })
+
+            // manejamos algún posible error
+            .catch( (err) => {
+                console.log(err.message)
+                res.status(statusError).json({
+                    success: false,
+                    message: err.message
+                })
+            })
+    })
 
     // Get de formularios
-    app.get('/formulario/:tipoFormulario/:subtipoFormulario/:token', (req, res) => {
+    app.get('/formulario/:empresa/:tipoFormulario/:subtipoFormulario/:token', (req, res) => {
         let data = {
             tipo_formulario_id: null,
             usuario_id: null,
         }
-        if(req.params.tipoFormulario.toString() === 'mantencion' && req.params.subtipoFormulario.toString() === 'hfc'){
+        if(req.params.empresa.toString() === 'claro' && req.params.tipoFormulario.toString() === 'mantencion' && req.params.subtipoFormulario.toString() === 'hfc'){
             data.tipo_formulario_id = 1
-        }else if(req.params.tipoFormulario.toString() === 'mantencion' && req.params.subtipoFormulario.toString() === 'dth'){
+        }else if(req.params.empresa.toString() === 'claro' && req.params.tipoFormulario.toString() === 'mantencion' && req.params.subtipoFormulario.toString() === 'dth'){
             data.tipo_formulario_id = 2
-        }else if(req.params.tipoFormulario.toString() === 'instalacion' && req.params.subtipoFormulario.toString() === 'hfc'){
+        }else if(req.params.empresa.toString() === 'claro' && req.params.tipoFormulario.toString() === 'instalacion' && req.params.subtipoFormulario.toString() === 'hfc'){
             data.tipo_formulario_id = 3
-        }else if(req.params.tipoFormulario.toString() === 'instalacion' && req.params.subtipoFormulario.toString() === 'dth'){
+        }else if(req.params.empresa.toString() === 'claro' && req.params.tipoFormulario.toString() === 'instalacion' && req.params.subtipoFormulario.toString() === 'dth'){
             data.tipo_formulario_id = 4
+        }else if(req.params.empresa.toString() === 'entel' && req.params.tipoFormulario.toString() === 'instalacion' && req.params.subtipoFormulario.toString() === 'dth'){
+            data.tipo_formulario_id = 6
         }
         let auth = new Promise ( (resolve, reject) => {
             global.validateToken(req.params.token, (response, err) => {
@@ -710,7 +861,7 @@ module.exports = (app) => {
             console.log(data.tipo_formulario_id)
             res.status(200).json({
                 success: true,
-                message: `Formularios de ${req.params.tipoFormulario} ${req.params.subtipoFormulario} del usuario con id = ${data.usuario_id}`,
+                message: `Formularios de ${req.params.tipoFormulario} ${req.params.subtipoFormulario} ${req.params.empresa} del usuario con id = ${data.usuario_id}`,
                 data: resolved
             })
         })
@@ -726,7 +877,7 @@ module.exports = (app) => {
     })
 
     // Get de formularios de desconexion
-    app.get('/formulario/desconexion/:token', (req, res) => {
+    app.get('/formulario/claro/desconexion/:token', (req, res) => {
         let data = {
             tipo_formulario_id: 5,
             usuario_id: null,
