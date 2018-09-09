a49294be4f697794071a1b817c336cb15bc0a7d4
JEMD <x_jemd_x@hotmail.com>
2018-09-09 18:47:25 -0300

    cambiando pass bdd

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'colseco',
+    password: 'Colseco',
     database: 'cfk_servicorp'
 })
 
