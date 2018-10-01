b602d9cea139c8b6965422c5ed412e7ee5ceba00
JEMD <x_jemd_x@hotmail.com>
2018-10-01 19:43:45 -0300

    prueba bugfix CORS

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 // Headers
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*, http://servicorp.xpass.cl")
     res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next()
