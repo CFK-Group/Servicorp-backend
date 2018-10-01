0f5a44a4c50ed8cb87a438a7518ac308ec08d42c
JEMD <x_jemd_x@hotmail.com>
2018-10-01 19:51:08 -0300

    prueba bugfix CORS 2

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(cors())
 
 // Headers
+app.options('*', cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*, http://servicorp.xpass.cl")
     res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT")
