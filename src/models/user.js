7929c8d6df265239ddb5ad7c22c8c57e6e59ac6c
javier muñoz <x_jemd_x@hotmail.com>
2019-02-05 20:41:56 -0300

    prueba de pool conection /2

--- src/models/user.js
+++ src/models/user.js
@@ -1,79 +1,84 @@
 let userModel = {};
 
 userModel.getUsers = (callback) => {
-    if(connection){
+    pool.getConnection(function(err, connection){
         connection.query('SELECT * FROM srv_usuario WHERE id NOT BETWEEN 1 AND 3 AND tipo_usuario NOT LIKE "%superadmin%" ORDER BY id',
             (err, row) => {
                 if(err){
-                    callback(err, null);
-                    console.log(`Error en getUsers: ${err.message}`);
+                    callback(err, null)
+                    console.log(`Error en getUsers: ${err.message}`)
                 }
                 if(!err){
-                    callback(null, row);
+                    callback(null, row)
                 }
             }
         )
-    }
-};
+        connection.release()
+    })
+}
 
 userModel.getUserById = (id, callback) => {
-    if(connection){
+    pool.getConnection(function(err, connection){
         connection.query('SELECT * FROM srv_usuario WHERE id=?', id,
             (err, row) => {
                 if(err){
-                    callback(err, null);
-                    console.log(`Error en getUserById: ${err.message}`);
+                    callback(err, null)
+                    console.log(`Error en getUserById: ${err.message}`)
                 }
                 if(!err){
-                    callback(null, row);
+                    callback(null, row)
                 }
             }
         )
-    }
-};
+        connection.release()
+    })
+}
 
 userModel.getUserByUsername = (user, callback) => {
-    if (connection) {
+    pool.getConnection(function(err, connection){
         connection.query('SELECT * FROM srv_usuario WHERE username = ?', user,
             (err, row) => {
                 if(err){
                     callback(err, null);
-                    console.log(`Error en getUserByUsername: ${err.message}`);
+                    console.log(`Error en getUserByUsername: ${err.message}`)
                 }
                 if(!err) callback(null, row);
             }
-        );
-    }
-};
+        )
+        connection.release()
+    })
+}
 
 userModel.createUser = (userData, callback) => {
-    if(connection){
+    pool.getConnection(function(err, connection){
         connection.query(
             'INSERT INTO srv_usuario SET ?', userData,
             (err, row) => {
                 if(err){
-                    callback(err, null);
-                    console.log(`Error en createUser: ${err.message}`);
-                }
-                if(!err) callback(null, row);
-            }
-        )
-    }
-};
-
-userModel.saveToken = (userData, callback) => {
-    if(connection){
-        let data = Object.keys(userData).map(key => userData[key]);
-        connection.query('UPDATE srv_usuario SET api_token=? WHERE id=?', data,
-            (err, row) => {
-                if(err){
-                    callback(err, null);
-                    console.log(`Error en saveToken: ${err.message}`);
+                    callback(err, null)
+                    console.log(`Error en createUser: ${err.message}`)
                 }
                 if(!err) callback(null, row)
             }
         )
-    }
-};
+        connection.release()
+    })
+}
 
-module.exports = userModel;
+userModel.saveToken = (userData, callback) => {
+    pool.getConnection(function(err, connection){
+        let data = Object.keys(userData).map(key => userData[key]);
+        connection.query('UPDATE srv_usuario SET api_token=? WHERE id=?', data,
+            (err, row) => {
+                if(err){
+                    callback(err, null)
+                    console.log(`Error en saveToken: ${err.message}`)
+                }
+                if(!err) callback(null, row)
+            }
+        )
+        connection.release()
+    })
+}
+
+module.exports = userModel
