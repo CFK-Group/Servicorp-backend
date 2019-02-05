ced9d608448c3ddd64854f4d31d28873011a24ff
javier muñoz <x_jemd_x@hotmail.com>
2019-02-05 18:36:34 -0300

    cambiando forma de conectar con la bdd
    reintentos de conexion cada 10 seg si no logra conectar con la bdd

---
+++ src/middlewares/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'colseco',
+    database: 'cfk_servicorp'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log('No se ha podido conectar a la bdd')
+        console.log(err.message)
+    }else{
+        console.log("Connectado a la bdd correctamente!")
+    }
+});
