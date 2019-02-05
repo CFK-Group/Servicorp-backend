04f3420d79764e00c0eabd2225e28e7d457e44ec
javier muñoz <x_jemd_x@hotmail.com>
2019-02-05 20:05:20 -0300

    prueba de pool conection

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,24 +1,8 @@
 const mysql = require('mysql')
 
-connection = mysql.createConnection({
+pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'colseco',
     database: 'cfk_servicorp'
 })
-
-conectarBD = () => {
-    connection.connect((err) => {
-        if (err) {
-            console.log('No se ha podido conectar a la bdd')
-            console.log(err.message)
-            setTimeout(() => {
-                conectarBD()
-            }, 1000 * 10)
-        }else{
-            console.log("Connectado a la bdd correctamente!")
-        }
-    })
-}
-
-conectarBD()
