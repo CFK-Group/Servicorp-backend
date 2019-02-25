fc266396af46ac3aef8840a1020e41f172465cc9
Javier Muñoz <x_jemd_x@hotmail.com>
2019-02-25 13:45:47 -0300

    bugfix nuevos logs/4

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -65,6 +65,7 @@ module.exports = (app) => {
     app.post('/login', (req, res) => {
         log.info('post: /login')
         log.info('req: ' + JSON.stringify(req.body))
+        log.info('res: ' + req.body.username)
         let username = req.body.username
         let password = req.body.password
         global.auth(username, password, res)
