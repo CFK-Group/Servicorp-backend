e5455e13cf0e739b7eb1d9a04fb3a9fe12f742e2
jemd15 <x_jemd_x@hotmail.com>
2019-06-17 20:30:58 -0400

    nuevos reportes /6

--- src/routes/formularioRoutes.js
+++ src/routes/formularioRoutes.js
@@ -3125,8 +3125,8 @@ module.exports = (app) => {
                             worksheet.columns = []
                             format.push({header:'Nº', key:'Nº'})
                             format.push({header:'Usuario', key:'Usuario'})
-                            format.push({header:'Número de OT Servicio', key:'Número de OT Servicio'})
                             format.push({header:'Folio', key:'Folio'})
+                            format.push({header:'Número de OT Servicio', key:'Número de OT Servicio'})
                             format.push({header:'Fecha', key:'Fecha'})
                             format.push({header:'Hora', key:'Hora'})
                             for (let i=2; i<res.length; i++){
@@ -3168,9 +3168,12 @@ module.exports = (app) => {
                             row.push(data[i].username)
                             /* row.push(data[1].respuesta)
                             row.push(data[0].respuesta) */
-                            if(data[i+1].glosa == 'FOLIO DE SERVICIO'){
-                                row.push(data[i+1].respuesta)
-                            }else if(data[i].glosa == 'OT SERVICORP'){
+                            if(data[i].glosa == 'FOLIO DE SERVICIO'){
+                                row.push(data[i].respuesta)
+                            }else{
+                                row.push('')
+                            }
+                            if(data[i].glosa == 'OT SERVICORP'){
                                 row.push(data[i].respuesta)
                             }else{
                                 row.push('')
