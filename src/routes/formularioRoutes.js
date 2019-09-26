be5517db19052ccc8310e7b8a4244df302bbbf41
Javier Muñoz <x_jemd_x@hotmail.com>
2019-09-26 00:36:36 -0300

    bugfix reportes /8

--- src/routes/formularioRoutes.js
+++ src/routes/formularioRoutes.js
@@ -1836,7 +1836,7 @@ module.exports = (app) => {
                     } else if (tipoFormulario === 'mantencion') {
                         for(let i=1; i<data.length; i++){
                             if(data[i].orden===49){
-                                data.splice(i,17)
+                                data.splice(i,15)
                             }
                         }
                     }
