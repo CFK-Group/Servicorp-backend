9d725bb3896957e3a3c03ad23332fe2d0c0524ec
Javier Muñoz <x_jemd_x@hotmail.com>
2019-10-01 18:46:45 -0300

    prueba borrar una columna más en reportes /5

--- src/routes/formularioRoutes.js
+++ src/routes/formularioRoutes.js
@@ -1834,7 +1834,7 @@ module.exports = (app) => {
                         }
                     } else if (tipoFormulario === 'mantencion') {
                         for(let i=1; i<data.length; i++){
-                            if(data[i].orden===47){
+                            if(data[i].orden===46){
                                 data.splice(i,17)
                             }
                         }
